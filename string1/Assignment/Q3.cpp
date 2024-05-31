51db9f16a009596f8bd25a0f8cda6a2f4c99cd4f
ABHISHEK KUMAR <abhishekkumar834438@gmail.com>
2024-05-31 22:36:59 +0530

    c

--- string1/Assignment/Q3.cpp
+++ string1/Assignment/Q3.cpp
@@ -6,26 +6,43 @@ Input : "abcdcba"
 Output : Yes
 */
 
-#include<iostream>
-using namespace std;
-bool isPalindrome(string s){
-    int j = 0;
-    int j = s.length()-1;
-    while(i<j){
-        if(s[i]!=s[j]) return false;
-        i++;
-        j--;
-    }
-    return true;
-}
-int main(){
-    string s;
-    getline(cin,s);
-    if(isPalindrome(s)==true)
-        cout<<"String is palindrome";
-    else
-        cout<<"String is not palindrome";
-}
+// #include<iostream>
+// using namespace std;
+// bool isPalindrome(string s){
+//     int j = 0;
+//     int j = s.length()-1;
+//     while(i<j){
+//         if(s[i]!=s[j]) return false;
+//         i++;
+//         j--;
+//     }
+//     return true;
+// }
+// int main(){
+//     string s;
+//     getline(cin,s);
+//     if(isPalindrome(s)==true)
+//         cout<<"String is palindrome";
+//     else
+//         cout<<"String is not palindrome";
+// }
 
-/*doubt see youTube 
-concept is not cleared by raghav sir*/
+// /*doubt see youTube 
+// concept is not cleared by raghav sir*/
+
+#include <bits/stdc++.h>
+using namespace std;
+bool check(string &s) {
+int i = 0, j = (int)s.size() - 1;
+while (i <= j) {
+if (s[i] != s[j]) return false;
+i++, j--;
+}
+}
+int main() {
+int n;
+cin >> n;
+string s;
+cin >> s;
+cout << (check(s) ? "YES" : "NO");
+}
