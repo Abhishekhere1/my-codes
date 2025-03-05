cababdc15a1b95de33177736a744ac889ed58eb4
ABHISHEK KUMAR <122744771+Abhishekhere1@users.noreply.github.com>
2025-03-05 18:34:09 +0530

    first

---
+++ stacks1/Assignment/q3.cpp
@@ -0,0 +1,23 @@
+/*
+
+What does this function do ?
+void fun(int n){
+stack<int> s;
+while (n > 0){
+s.push(n%2);
+n = n/2;
+}
+while (!s.empty()){
+cout<<s.top();
+s.pop();
+}
+}
+Prints binary representation of n in reverse order
+Prints binary representation of n
+Print the value of Log n
+Print the value of Log n in reverse order
+
+
+ans : option 2: Prints binary representation of n.
+
+*/
